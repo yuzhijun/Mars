ee6a347c0aa2d0c212d4e1379b8d13247369ef11
yuzhijun <664281688@qq.com>
2018-04-20 16:27:15 +0800

    增加新的apk下载更新

--- mars-consumer/src/main/java/com/winning/mars_consumer/MarsEntrance.java
+++ mars-consumer/src/main/java/com/winning/mars_consumer/MarsEntrance.java
@@ -1,7 +1,12 @@
 package com.winning.mars_consumer;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
 
+import com.winning.mars_consumer.utils.DownloadUtil;
 import com.winning.mars_generator.Mars;
 import com.winning.mars_generator.core.modules.battery.Battery;
 import com.winning.mars_generator.core.modules.cpu.Cpu;
@@ -12,6 +17,10 @@ import com.winning.mars_generator.core.modules.leak.Leak;
 import com.winning.mars_generator.core.modules.sm.Sm;
 import com.winning.mars_generator.core.modules.thread.deadlock.DeadLock;
 import com.winning.mars_generator.core.modules.traffic.Traffic;
+import com.winning.mars_generator.utils.LogUtil;
+
+import java.io.File;
+import java.lang.ref.WeakReference;
 
 /**
  * Created by yuzhijun on 2018/4/10.
@@ -51,5 +60,50 @@ public class MarsEntrance {
         .install(Traffic.class);
 
         MarsConsumer.consume(context);
+    }
+
+    private void download(Context context,String downloadUrl){
+        DownloadUtil.download(downloadUrl,new DownloadHandler(this,context));
+    }
+
+    private void installApk(Context context,String apkPath) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setDataAndType(Uri.fromFile(new File(apkPath)), "application/vnd.android.package-archive");
+        context.startActivity(intent);
+    }
+
+    static class DownloadHandler extends Handler {
+        WeakReference<MarsEntrance> mMarsEntranceReference;
+        Context mContext;
+
+        DownloadHandler(MarsEntrance marsEntrance,Context context) {
+            mMarsEntranceReference= new WeakReference<>(marsEntrance);
+            mContext = context;
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            final MarsEntrance marsEntrance = mMarsEntranceReference.get();
+
+            switch (msg.what) {
+                case DownloadUtil.DOWNLOAD_ERROR: {
+                    LogUtil.d("DL", "error" + msg.obj);
+                }
+                break;
+                case DownloadUtil.DOWNLOAD_CANCEL:{
+                    LogUtil.d("DL", "cancel thread id:" + msg.obj);
+                }
+                break;
+                case DownloadUtil.DOWNLOAD_FINISH: {
+                    marsEntrance.installApk(mContext,(String) msg.obj);
+                }
+                break;
+                case DownloadUtil.DOWNLOADING: {
+                }
+                break;
+            }
+        }
     }
 }
